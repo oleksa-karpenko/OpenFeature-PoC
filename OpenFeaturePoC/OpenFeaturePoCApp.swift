e7d537e9bb1c025e95b00019103ea03a6fbaf2b4
Oleksandr Karpenko <okarp@softserveinc.com>
2025-06-11 15:08:19 +0300

    Initial Commit

---
+++ OpenFeaturePoC/OpenFeaturePoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenFeaturePoCApp.swift
+//  OpenFeaturePoC
+//
+//  Created by Oleksandr Karpenko on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct OpenFeaturePoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpenFeaturePoCTests/OpenFeaturePoCTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenFeaturePoCTests.swift
+//  OpenFeaturePoCTests
+//
+//  Created by Oleksandr Karpenko on 11.06.2025.
+//
+
+import Testing
+@testable import OpenFeaturePoC
+
+struct OpenFeaturePoCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
